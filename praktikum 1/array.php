bcc8f0c40e21c0fd9668fb8efa43ac21e3a40516
lav713088-web <lav713088@gmail.com>
2025-09-10 14:08:49 +0700

    Add files via upload

---
+++ praktikum 1/array.php
@@ -0,0 +1,9 @@
+<?php
+$mhs1 = [
+    'nama' => 'Anggraeni',
+    'nim' => '0112',
+    'jurusan' => 'Sistem informasi',
+];
+
+//memamggil array asosiatif
+echo 'Nama: ' . $mhs1['nama'];
---
+++ praktikum 1/funtion.php
@@ -0,0 +1,11 @@
+<?php
+// fungtion untuk menghitung Luas segitiga
+function Luas_segitiga($alas, $tinggi){
+    $luas = 0.5 + $alas = $tinggi;
+    echo $luas;
+
+}
+
+
+//panggil function
+Luas_segitiga(2, 10);
